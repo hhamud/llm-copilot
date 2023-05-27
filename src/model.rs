92eafced4895a86a6e72046cdbf9b67c74444245
User <53880692+hhamud@users.noreply.github.com>
2023-05-27 03:18:48 +0100

    feat: add functions to download models from hf

--- src/model.rs
+++ src/model.rs
@@ -1,3 +1,4 @@
+use git2::Repository;
 use llm::{
     load, load_progress_callback_stdout, InferenceFeedback, InferenceRequest, InferenceResponse,
 };
@@ -70,5 +71,83 @@ impl Model {
         }
 
         data
+    }
+
+    pub fn download(&self, hf: &str) -> Result<(), git2::Error> {
+        let hf_repo = HuggingFaceRepo::from_str(hf)
+            .map_err(|_| git2::Error::from_str("Invalid Hugging Face repository format"))?;
+
+        let url = format!("https://huggingface.co/{}", hf_repo.to_string());
+        let local_path = Path::new(".models").join(&hf_repo.repo);
+
+        // Create directory if it doesn't exist
+        if !Path::new(".models").exists() {
+            fs::create_dir(".models")?;
+        }
+
+        let _repo = match Repository::clone(&url, &local_path) {
+            Ok(repo) => repo,
+            Err(e) => panic!("Failed to clone: {}", e),
+        };
+
+        Ok(())
+    }
+
+    pub fn load_from_repo<T>(&self, hf: &str) -> Result<(), Error>
+    where
+        T: llm::KnownModel + 'static,
+    {
+        let hf_repo = HuggingFaceRepo::from_str(hf)
+            .map_err(|err| Error::new(std::io::ErrorKind::InvalidData, err))?;
+
+        let path = PathBuf::from(".models").join(&hf_repo.repo);
+
+        if let Ok(entries) = fs::read_dir(path) {
+            for entry in entries {
+                if let Ok(entry) = entry {
+                    // If the entry is a file and it has extension .bin
+                    if entry.path().is_file()
+                        && entry.path().extension().unwrap_or_default() == "bin"
+                    {
+                        let llama = self.new::<T>(&entry.path());
+                        // Now you can use the llama model
+                        //...
+                        return Ok(());
+                    }
+                }
+            }
+        }
+
+        Err(Error::new(
+            std::io::ErrorKind::NotFound,
+            "Model file not found",
+        ))
+    }
+}
+
+pub struct HuggingFaceRepo {
+    username: String,
+    repo: String,
+}
+
+impl HuggingFaceRepo {
+    pub fn new<S: Into<String>>(username: S, repo: S) -> Self {
+        Self {
+            username: username.into(),
+            repo: repo.into(),
+        }
+    }
+
+    pub fn from_str<S: AsRef<str>>(s: S) -> Result<Self, &'static str> {
+        let parts: Vec<&str> = s.as_ref().split('/').collect();
+        if parts.len() != 2 {
+            Err("Invalid format. Expected 'username/repo'.")
+        } else {
+            Ok(Self::new(parts[0], parts[1]))
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        format!("{}/{}", self.username, self.repo)
     }
 }
